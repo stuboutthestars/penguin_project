004073e43bc6d2574107bdc6eac71ef4eb6ecd29
stuboutthestarsglobal user.name stuboutthestars <s1969681@ed.ac.uk>
2023-09-27 15:00:15 +0100

    added subset (female only data) and saved this as a new dataset

--- 1_Penguin_Analysis.R
+++ 1_Penguin_Analysis.R
@@ -17,3 +17,8 @@ summary(bill_sex_model)
 #custom <- c("Adelie" = "#FF66CC", "Chinstrap" = "#9900CC", "Gentoo" = "#330066")
 ggplot(na.omit(penguin_data), aes(x = sex, y = bill_length_mm, colour = species)) + geom_boxplot() + stat_smooth(method = "lm")
 ggsave("figs/1_sex_bill_length_regression.png")
+
+#subesttign the data to only look at females of each species
+penguins_female <- subset(penguin_data, sex == "female")
+#saving this subset
+write_tsv(penguins_female, "results/1_penguin_female_only.txt")
